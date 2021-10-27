f2c25c9df40c5beec9fc284137399e6122e928e6
Antal Szava <antalszava@gmail.com>
2021-10-27 11:55:36 -0400

    clean html def

--- .github/workflows/html_parser.py
+++ .github/workflows/html_parser.py
@@ -11,9 +11,16 @@ class DemoOutputParser(HTMLParser):
     
     def __init__(self):
         self.data = []
+        '''Stores the output data of demonstrations extracted from the HTML
+        file of a demonstration.'''
+
+        self.is_demo_output = False
+        '''Specifies whether the tag that is being parsed is the output of a cell
+        in the demo.'''
+
         super().__init__()
     
-    def handle_starttag(self, tag, attrs):
+    def handle_starttag(self, attrs):
         for attr in attrs:
             sphinx_script_output_tag = "sphx-glr-script-out"
             out = any(sphinx_script_output_tag in a for a in attr if a is not None)
@@ -23,22 +30,7 @@ class DemoOutputParser(HTMLParser):
             if "class" in attr and out:
                 self.is_demo_output = True
 
-    def handle_endtag(self, tag):
-        pass
-
     def handle_data(self, data):
         if self.is_demo_output:
             self.data.append(data)
             self.is_demo_output = False
-
-    def handle_comment(self, data):
-        pass
-
-    def handle_entityref(self, name):
-        pass
-
-    def handle_charref(self, name):
-        pass
-
-    def handle_decl(self, data):
-        pass
